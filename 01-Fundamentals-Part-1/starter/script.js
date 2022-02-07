55ad77c5242b41638464e696f71cd26aaf9701ed
chamara <chamara.weerasinghe@my.sliit.lk>
2022-02-07 19:43:32 +0530

    adding ternery oparator

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,62 +1,32 @@
-// type conversion
+const age = 23;
+
+// if else in one line
+// this is also called ternary operator because of three part condition -> true expression -> false expression
+const drink =
+  age >= 18
+    ? console.log("i like to drink wine")
+    : console.log("i like to drink water");
+
+// operator is always return a va lue => expression
+
+// using if
+
+let drink2;
+
+if (age >= 18) {
+  drink2 = "wine";
+} else {
+  drink2 = "water";
+}
+
+console.log(drink2);
+
+// we can use it inside the template literal
 
 console.log(
-  "``````````````````````Number````````````````````````````````````````````````"
-);
-// Number
-
-const inputYear = "1994";
-console.log(typeof Number(inputYear), Number(inputYear));
-console.log(typeof (Number(inputYear) + 12), Number(inputYear) + 12);
-// -> 199412
-// -> 1994 + 12 = 2006
-// not a number -> NaN
-console.log(Number("number"));
-console.log(typeof NaN);
-
-console.log(
-  "``````````````````````Number````````````````````````````````````````````````"
-);
-
-console.log(
-  "``````````````````````String````````````````````````````````````````````````"
-);
-
-// String
-
-console.log(typeof String(23), String(23), "||", typeof 23, 23);
-
-console.log(
-  "``````````````````````String````````````````````````````````````````````````"
-);
-
-// type coercion
-console.log(
-  `start ---------------------type coercion------------------------------------`
-);
-
-console.log("i am " + 23 + "years old");
-console.log(`'12' - '1' -> `, "12" - "1");
-console.log(`'12' + '1' -> `, "12" + "1");
-console.log(`'12' * '5' -> `, "12" * "5");
-console.log(`'12' / '2' -> `, "12" / "2");
-
-// logical operators
-console.log(` '12' > '2' ->`, "12" > "2");
-
-let n = "1" + 1;
-n = n - 1;
-console.log(
-  `let n = "1" + 1;
-   n = n - 1;
-   log(n)
-`,
-  n
-);
-
-console.log(2 + 3 + 4 + "5");
-console.log("10" - "4" - "3" + "5");
-
-console.log(
-  `end ---------------------type coercion------------------------------------`
+  `${
+    age >= 18
+      ?"i like to drink wine"
+      : "i like to drink water"
+  }`
 );
