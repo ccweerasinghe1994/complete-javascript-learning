0dec460e646ad7d6d004aa9540f40a22eb86ba89
chamara <chamara.weerasinghe@my.sliit.lk>
2022-02-07 13:53:10 +0530

    varibales and values rules and conventions

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,6 +1,38 @@
-let js = "amazing";
-if (js === "amazing") {
-  alert("javascript is amazing");
-}
+// these are values
+console.log("this is a value");
+console.log(23);
 
-console.log(45 - 8 * 8);
+// we can store them in variables
+
+// declaring a variable
+
+let firstName = "kate";
+// camel case
+// snake case
+// let 3yeas can't do this
+// let test&me can't do that
+
+// variable can only contain letters _ numbers $
+// variables can't use reserved key words in the variable
+// name is a reserved key word as well.
+
+// so don't use it if you can.
+// let name = 23;
+
+// don't start with a upperCase letters
+// let Person = 12;
+
+// naming constants
+let API = 12;
+
+// write distinctive names
+
+// good
+let myFirstJob = "teacher";
+let mySecondJob = "programmer";
+// bad
+let job1 = "teacher";
+let job2 = "programmer";
+
+
+
