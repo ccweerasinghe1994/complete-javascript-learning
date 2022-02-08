6659b46f776a42cb04726338b4b54dc9cd00898f
chamara <chamara.weerasinghe@my.sliit.lk>
2022-02-08 05:43:21 +0530

    added arrays scrit mode array methods

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,109 @@
+"use strict";
+
+// function calcAge(birthYear) {
+//   return 2037 - birthYear;
+// }
+
+// function yearsUntilRetirement(birthYear, firstName) {
+//   const age = calcAge(birthYear);
+//   const retirement = 65 - age;
+
+//   if (retirement > 0) return `${firstName} retires in ${retirement} years`;
+
+//   return `${firstName} is already retired`;
+//   // return retirement;
+// }
+
+// console.log(yearsUntilRetirement(1934, "chamara"));
+
+/ /; ////////////////////// ARRAYS //////////////////
+
+// const friend1 = "bob";
+// const friend2 = "andrew";
+// const friend3 = "rock";
+
+// const friends = ["bob", "andrew", "rock"];
+
+// console.log(friends);
+
+// const friends_with_Array = new Array("bob", "andrew", "rock");
+// console.log(friends_with_Array);
+
+// // accessing data
+// console.log(friends[0]);
+// console.log(friends[1]);
+// console.log(friends[2]);
+
+// // lenth of an array
+
+// console.log(friends.length);
+
+// // getting the last element
+
+// console.log(friends[friends.length - 1]);
+
+// // different type of arrays
+// const me = ["rock", 2022 - 1982, "actor"];
+// console.log(me);
+
+// const calcAge = function (birthAge) {
+//   return 2022 - birthAge;
+// };
+
+// const years = [1994, 1995, 1996, 2000];
+
+// const age1 = calcAge(years[0]);
+// const age2 = calcAge(years[1]);
+// const age3 = calcAge(years[2]);
+
+// console.log(age1);
+// console.log(age2);
+// console.log(age3);
+
+// // [] we can you expression
+// const newArray = [years[years.length - 1], calcAge(years[0])];
+// console.log(newArray);
+
+// ///////////////////  ARRAYS //////////////////////////////
+
+// const friends = ["bob", "andrew", "rock"];
+
+// console.log(`start ---------------------- ARRAY-----------------`);
+// console.log(friends);
+// // adding
+// console.log(`start ---------------------- adding-----------------`);
+// // end
+// const push = friends.push("andrew");
+// console.log("push", push);
+// console.log(friends);
+// // begin
+// const unshift = friends.unshift("json");
+// console.log("unshift", unshift);
+// console.log(friends);
+// console.log(`end ---------------------- adding-----------------`);
+// console.log(`start ---------------------- removing-----------------`);
+// // removing
+// console.log(friends);
+// const pop = friends.pop();
+// console.log("pop", pop);
+// console.log(friends);
+// const shift = friends.shift();
+// console.log("shift", shift);
+// console.log(friends);
+
+// console.log(`end ---------------------- removing-----------------`);
+// // search
+// const index = friends.indexOf("bob");
+// const index_2 = friends.indexOf("b");
+// console.log(index);
+// console.log(index_2);
+// // strict matching
+// const includes = friends.includes("bob");
+// console.log(includes);
+// console.log(`end ---------------------- ARRAY-----------------`);
+
+
+// ////////////////////////  OBJECTS ////////////////////////////////
+
+
+
