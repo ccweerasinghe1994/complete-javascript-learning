57898224125b625fc46380b0e7c06ce065f99ac2
chamara <chamara.weerasinghe@my.sliit.lk>
2022-02-08 07:52:48 +0530

    added while loop objects methods for loops

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -102,8 +102,202 @@
 // console.log(includes);
 // console.log(`end ---------------------- ARRAY-----------------`);
 
-
 // ////////////////////////  OBJECTS ////////////////////////////////
 
+// const me = ["rock", 2022 - 1982, "actor", ["gagni", "shan", "gayan"]];
 
+// order doesn't matter
+// const meObj = {
+//   first_name: "chamara",
+//   last_name: "weerasinghe",
+//   birthYear: 2022 - 1982,
+//   job: "full stack developer",
+//   friends: ["gagni", "shan", "gayan"],
+// };
 
+// console.log(meObj);
+// // .dot notation -> strict value
+// console.log(meObj.last_name);
+// // [] notation -> expression
+// console.log(meObj["last_name"]);
+
+// const nameKey = "_name";
+
+// console.log(meObj["first" + nameKey]);
+// console.log(meObj["last" + nameKey]);
+
+// const interestedIn = prompt(
+//   "what do you want to know about chamara ? first_name,last_name,birthYear,job,friends"
+// );
+// console.log(interestedIn);
+// console.log(meObj[interestedIn]);
+
+// const friendFunction = (interestedIn) => {
+//   console.log("i am running");
+//   if (interestedIn === "friends") {
+//     return `${meObj.first_name} has ${meObj.friends.length} friends and his best friend is ${meObj.friends[0]}`;
+//   }
+// };
+
+// if (meObj[interestedIn]) {
+//   console.log(meObj[interestedIn]);
+//   console.log(friendFunction(interestedIn));
+// } else {
+//   console.log(
+//     `wrong request!! request something first_name,last_name,birthYear,job,friends`
+//   );
+// }
+
+// // adding more properties
+
+// meObj.location = "sti lanka";
+// meObj["twitter"] = "@ccweerasinghe1994";
+
+// OBJECT METHODS /////////////////////////////////////////////////
+
+// const me = {
+//   first_name: "chamara",
+//   last_name: "weerasinghe",
+//   birthYear: 1982,
+//   job: "full stack developer",
+//   friends: ["gagni", "shan", "gayan"],
+//   hasDriversLicense: true,
+
+//   //   //   this is  confusing
+//   //   calcAge: function (birthYear) {
+//   //     return 2037 - birthYear;
+//   //   },
+
+//   //   // this has to be a expression
+//   //   // if you run this 100 times
+//   //   // it will execute 100 times
+//   //   // this is not efficient
+
+//   //   calcAge: function () {
+//   //     return 2037 - this.birthYear;
+//   //   },
+//   // now at the first run it will add the age property to the this.object
+//   calcAge: function () {
+//     this.age = 2037 - this.birthYear;
+//     return this.age;
+//   },
+//   getSummery: function () {
+//     return `${
+//       this.first_name
+//     } is a ${this.calcAge()} years old teacher and he ${
+//       this.hasDriversLicense ? "has" : "does't have"
+//     } a driver's license`;
+//   },
+// };
+// console.log(me);
+
+// console.log(me.getSummery());
+
+// LOOPS ////////////////////////////////////////////////////
+
+// console.log(`lifting weights rep 1`);
+// console.log(`lifting weights rep 2`);
+// console.log(`lifting weights rep 3`);
+// console.log(`lifting weights rep 4`);
+// console.log(`lifting weights rep 5`);
+// console.log(`lifting weights rep 6`);
+// console.log(`lifting weights rep 7`);
+// console.log(`lifting weights rep 8`);
+// console.log(`lifting weights rep 9`);
+// console.log(`lifting weights rep 10`);
+
+// for (let index = 1; index <= 10; index++) {
+//   console.log(`lifting weights rep ${index}`);
+// }
+
+// const types = [];
+// const me = [
+//   "chamara",
+//   "weerasinghe",
+//   2022 - 1982,
+//   "actor",
+//   ["gagni", "shan", "gayan"],
+// ];
+
+// for (let i = 0; i < me.length; i++) {
+//   console.log(me[i], typeof me[i]);
+
+//   types.push(typeof me[i]);
+// }
+// console.log(types);
+
+// const years = [1994, 1995, 1996, 1997];
+// const ages = [];
+
+// for (let index = 0; index < years.length; index++) {
+//   const element = years[index];
+//   ages.push(2022 - element);
+// }
+
+// console.log(ages);
+
+// // continue and break
+
+// //  continue
+// console.log(`-----------------------------------------------------`);
+// for (let i = 0; i < me.length; i++) {
+//   if (typeof me[i] !== "string") continue;
+//   console.log(me[i], typeof me[i]);
+// }
+// console.log(`-----------------------------------------------------`);
+
+// // break
+// for (let i = 0; i < me.length; i++) {
+//   if (typeof me[i] === "number") break;
+//   console.log(me[i], typeof me[i]);
+// }
+
+// console.log(`-----------------------------------------------------`);
+
+// const me = [
+//   "chamara",
+//   "weerasinghe",
+//   2022 - 1982,
+//   "actor",
+//   ["gagni", "shan", "gayan"],
+// ];
+
+// console.log(me);
+// const reverse = [];
+// for (let i = 1; i < me.length; i++) {
+//   reverse.push(me[me.length - i]);
+// }
+
+// console.log(reverse);
+
+// // loop inside a loop
+
+// for (let i = 0; i < me.length; i++) {
+//   console.log("outer loop " + i);
+//   for (let j = 0; j < 4; j++) {
+//     console.log("              inner loop " + j);
+//   }
+// }
+
+// while loop
+
+// counter
+// condition
+// increment
+
+let rep = 1;
+while (rep <= 10) {
+  console.log("running");
+  rep++;
+}
+
+let dice = Math.trunc(Math.random() * 6) + 1;
+// we don't know how many times we will dice
+// there for we don't know the counter
+while (dice !== 6) {
+  console.log(`your rolled a  ${dice}`);
+  dice = Math.trunc(Math.random() * 6) + 1;
+  if (dice === 6) {
+    console.log("loop is about to end");
+  }
+}
