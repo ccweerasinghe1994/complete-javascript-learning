b91c35019b33e2985a7c2752651760909a3c2fef
chamara <chamara.weerasinghe@my.sliit.lk>
2022-02-11 05:03:57 +0530

    object and array descrturing

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,51 +1,60 @@
 'use strict';
 
-// this selector argument is same as css selectors
-
-// console.log(document.querySelector('.message').textContent);
-
-// document.querySelector('.message').textContent = `correct answer🎉`;
-
-// console.log(document.querySelector('.message').textContent);
-
-// document.querySelector('.number').textContent = 13;
-// document.querySelector('.score').textContent = 10;
-// document.querySelector('.guess').value = 10;
-
 let score = 20;
+let highScore = 0;
 
 const convertToNumber = number => Number(number);
 const generateRandomNumber = () => Math.trunc(Math.random() * 20 + 1);
 
-const secretNumber = generateRandomNumber();
+let secretNumber = generateRandomNumber();
 
-document.querySelector('.number').textContent = secretNumber;
 // event listener
 
+function getMessage(message) {
+  document.querySelector('.message').textContent = message;
+}
 document.querySelector('.check').addEventListener('click', () => {
   const guess = document.querySelector('.guess').value;
   const numberGuess = convertToNumber(guess);
+
   if (!numberGuess) {
-    document.querySelector('.message').textContent = ` ⛔ No Number`;
+    // document.querySelector('.message').textContent = `⛔ No Number`;
+    getMessage('⛔ No Number');
   } else if (numberGuess === secretNumber) {
     document.querySelector('.message').textContent = `🙌 correct answer 🎉🎆`;
-  } else if (numberGuess > secretNumber) {
-    if (score > 1) {
-      document.querySelector('.message').textContent = `📈 Too High`;
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent = `🧨you lost`;
-      document.querySelector('.score').textContent = 0;
+    // adding styles
+    // always camel case
+    // value always a string
+    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
     }
-  } else if (numberGuess < secretNumber) {
+  } else if (numberGuess !== secretNumber) {
     if (score > 1) {
-      document.querySelector('.message').textContent = `📉 Too Low`;
+      getMessage(numberGuess > secretNumber ? `📈 Too High` : `📉 Too Low`);
       score--;
       document.querySelector('.score').textContent = score;
     } else {
-      document.querySelector('.message').textContent = `🧨you lost`;
+      getMessage(`🧨you lost`);
       document.querySelector('.score').textContent = 0;
     }
   }
+});
+
+// again functionality
+
+document.querySelector('.again').addEventListener('click', () => {
+  console.log('running');
+  score = 20;
+  secretNumber = generateRandomNumber();
+  document.querySelector('.score').textContent = score;
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+  getMessage('Start guessing...');
 });
--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -26,4 +26,97 @@ const restaurant = {
       close: 24,
     },
   },
+  order: function (side, main) {
+    return [this.starterMenu[side], this.mainMenu[main]];
+  },
+  orderDelivery: function ({ time = '121', side, main }) {
+    return `${
+      this.name
+    } will deliver your order in ${time} minutes. Your order is: ${this.order(
+      side,
+      main
+    ).join(', ')}`; // abc will deliver your order in x minutes. Your order is: x, y
+  },
 };
+
+const values = restaurant.orderDelivery({
+  time: '12:00',
+  address: 'Via Angelo Tavanti 23, Firenze, Italy',
+  mainIndex: 1,
+  starterIndex: 0,
+});
+
+console.log(values); // Classico Italiano will deliver your order in 12:00 minutes. Your order is: Caprese Salad, Pizza
+
+// console.log(restaurant.categories);
+
+// let [main, , secondary] = restaurant.categories; // [ "Italian", "Pizzeria", "Vegetarian", "Organic" ]
+
+// console.log(main, secondary); //Italian Vegetarian
+
+// // flip the values
+
+// [main, secondary] = [secondary, main];
+
+// console.log(main, secondary); // Vegetarian Italian
+
+// // returning two values from a function
+// const [sideMenu, mainMenu] = restaurant.order(1, 2);
+// console.log(sideMenu, mainMenu);
+
+// nested array desc
+
+// const array = [1, 2, 2, [12, 4, 2]];
+
+// const [a, , , [b, c, d]] = array;
+
+// console.log(a, b, c, d);
+
+// // default values
+
+// const [a = 1, b = 2, c = 3] = [5, 7];
+
+// console.log(a, b, c);
+
+// objects --------------------------
+// order doesn't matter
+const { name, openingHours, categories } = restaurant;
+// giving different names to the same values
+// this is very helpful when you are using third party apis
+const {
+  name: restaurantName,
+  openingHours: restaurantHours,
+  categories: restaurantCategories,
+} = restaurant;
+// console.log(restaurantName, restaurantHours, restaurantCategories);
+
+// // setting a default value
+
+// const { menu = [], starterMenu: starters = [] } = restaurant;
+
+// console.log(menu, starters); // Array [] Array(4) [ "Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad" ]
+
+// // mutating variables
+
+// let a = 1;
+// let b = 2;
+// let c = 3;
+
+// const obj = {
+//   a: 23,
+//   b: 34,
+//   c: 45,
+// };
+
+// console.log(a,b); // 1 2
+
+// // {} = obj; // this is not allowed because {} represent a code block
+
+// ({ a, b } ) = obj;
+
+// console.log(a,b); // 23 34
+
+// // nested objects destructuring
+
+// const {openingHours:{fri,sat,thu}} = restaurant;
+// console.log(fri,sat,thu); // { open: 11, close: 23 } { open: 0, close: 24 } { open: 12, close: 22 }
